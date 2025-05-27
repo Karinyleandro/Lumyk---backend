fb40714d5d408ffd9cecba7a58edd8c83275d0af
Kariny Leandro <karinysilva.estudo@gmail.com>
2025-05-27 10:15:06 -0300

    testando post

--- backend/app/controllers/EnderecoController.py
+++ backend/app/controllers/EnderecoController.py
@@ -16,15 +16,19 @@ def buscar_endereco_por_id(id):
 
 def criar_endereco(data):
     usuario_id = get_jwt_identity()
+    
+    if not usuario_id:
+            return ({'mensagem': 'Usuário não autenticado.'}), 401
+
 
     novo_endereco = Endereco(
-        id=str(uuid.uuid4()),
-        id_usuario=usuario_id,
-        id_estado=data.get['id_estado'],
-        numero=data.get['numero'],
-        bairro=data.get['bairro'],
-        rua=data.get['rua']
-    )
+    id=str(uuid.uuid4()),
+    id_usuario=usuario_id,
+    id_estado=data.get('id_estado'),
+    numero=data.get('numero'),
+    bairro=data.get('bairro'),
+    rua=data.get('rua')
+)
 
     db.session.add(novo_endereco)
     db.session.commit()
